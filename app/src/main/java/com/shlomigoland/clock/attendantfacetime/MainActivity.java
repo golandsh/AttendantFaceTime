46cb98e382f310d71da3ed7dcdfcbd53b69d45b4
Shlomi Goland <golandsh@hotmail.com>
2017-03-02 17:42:56 +0200

    Created my own scanner

--- app/src/main/java/com/shlomigoland/clock/attendantfacetime/MainActivity.java
+++ app/src/main/java/com/shlomigoland/clock/attendantfacetime/MainActivity.java
@@ -3,19 +3,33 @@ package com.shlomigoland.clock.attendantfacetime;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback, ZXingScannerView.ResultHandler {
 
     private static final String TAG = "MainActivity";
 
     private Context mContext;
     private RelativeLayout mLoRelMain;
     private Button mBtnScan;
+
+    private ZXingScannerView mScannerView;
+
+    private SurfaceView topSurfaceView;
+    //private SurfaceView bottomSurfaceView;
+    private SurfaceHolder topSurfaceHolder;
+    //private SurfaceHolder bottomSurfaceHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +39,15 @@ public class MainActivity extends AppCompatActivity {
 
         initComponents();
         initComponentListeners();
+        initSurfaceViews();
+    }
+
+    private void initSurfaceViews() {
+        /*bottomSurfaceView = (SurfaceView)findViewById(R.id.bottomSurfaceView);
+        bottomSurfaceHolder = bottomSurfaceView.getHolder();
+        bottomSurfaceHolder.addCallback(this);*/
+        topSurfaceView = (SurfaceView)findViewById(R.id.topSurfaceView);
+        topSurfaceView.setZOrderOnTop(true);
     }
 
     private void initComponents(){
@@ -46,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private void startScan(){
-        try {
+        /*try {
             Intent intent = new Intent("com.google.zxing.client.android.SCAN");
             intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
 
@@ -58,7 +81,11 @@ public class MainActivity extends AppCompatActivity {
             Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
             startActivity(marketIntent);
 
-        }
+        }*/
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
     }
 
     @Override
@@ -73,5 +100,26 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        mScannerView.stopCamera();
+        mScannerView.setActivated(false);
     }
 }
